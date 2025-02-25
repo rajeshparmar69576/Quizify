f9316924a67fe8b92f881ff0ee0b17cf6dca5b3c
rajeshparmar69576 <rp69576@gmail.com>
2025-02-25 15:41:17 +0530

    done with frontend and backend initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div className="bg-pink-400">
+        Checking tailwind wit vite installation
+      </div>
+    </>
+  )
+}
+
+export default App
